602c4a5188c007e6d5f89c9968be915ec3995971
Aishwarya2422005 <141259345+Aishwarya2422005@users.noreply.github.com>
2024-02-06 21:36:01 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
-
+import warnings
+warnings.filterwarnings('ignore')
 st.set_page_config(page_title="Superstore Analysis", page_icon=":bar_chart:",layout="wide")
 
 st.title(" 	:computer: SuperStore EDA")
